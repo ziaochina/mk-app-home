aaf00c6eff3fec1462f162d55648a9be3d90129a
ziaochina <ziaochina@gmail.com>
2017-09-28 00:04:33 +0800

    zlj

--- example/mock.js
+++ example/mock.js
@@ -1,16 +1,5 @@
 
-
 import './apps/mk-app-home/apps/mk-app-home-chart/mock.js';
-
-
-
 import './apps/mk-app-home/apps/mk-app-home-list/mock.js';
-
-
-
 import './apps/mk-app-home/apps/mk-app-home-shortcuts/mock.js';
-
-
-
 import './apps/mk-app-home/mock.js';
-
